15ea2fd2e14de08798820a8def35d074deefd9ff
Dasom Park <ektha4306@gmail.com>
2021-10-18 16:06:24 +0900

    Modified palette UI

--- app/src/main/java/com/example/memoapp/DrawActivity.kt
+++ app/src/main/java/com/example/memoapp/DrawActivity.kt
@@ -1,14 +1,8 @@
 package com.example.memoapp
 
-import android.app.Dialog
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
-import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.example.memoapp.databinding.ActivityDrawBinding
 import com.example.memoapp.dialog.ThemeDialog
@@ -17,7 +11,7 @@ import com.example.memoapp.entity.DrawCanvas
 class DrawActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDrawBinding
 
-    private lateinit var canvas : DrawCanvas
+    private lateinit var canvas: DrawCanvas
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_draw)
@@ -36,7 +30,7 @@ class DrawActivity : AppCompatActivity() {
     }
 
     private val onClickListener = View.OnClickListener() {
-        when(it.id) {
+        when (it.id) {
             R.id.iv_menu -> {
 
             }
@@ -47,29 +41,25 @@ class DrawActivity : AppCompatActivity() {
                 canvas.redo()
             }
             R.id.iv_tool -> {
-                if(canvas.currentTool == DrawCanvas.Tool.PEN) {
+                if (canvas.currentTool == DrawCanvas.Tool.PEN) {
                     binding.ivTool.setImageResource(R.drawable.round_pencil_24)
                     canvas.changeTool(DrawCanvas.Tool.ERASER)
-                }
-                else if(canvas.currentTool == DrawCanvas.Tool.ERASER) {
+                } else if (canvas.currentTool == DrawCanvas.Tool.ERASER) {
                     binding.ivTool.setImageResource(R.drawable.ic_eraser_24)
                     canvas.changeTool(DrawCanvas.Tool.PEN)
                 }
             }
             R.id.iv_color_and_type -> {
-//                var intent = Intent(this, ThemeDialog::class.java)
-//                startActivity(intent)
-//                val dialog = LayoutInflater.from(this).inflate(R.layout.dialog_theme, null)
-//                val builder = AlertDialog.Builder(this)
-//                    .setView(dialog)
-//                val alertDialog = builder.show()
-                val dialog = Dialog(this)
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-                dialog.setContentView(R.layout.dialog_theme)
-                val params = dialog.window!!.attributes
-                params.width = WindowManager.LayoutParams.WRAP_CONTENT
-                params.height = WindowManager.LayoutParams.WRAP_CONTENT
-                dialog.window!!.attributes = params
+                val dialog = ThemeDialog(this,
+                    onToolItemSelected = { itemIndex : Int ->
+                        //TODO : 선택된 툴로 변경
+                    },
+                    onColorItemSelected = { itemIndex : Int ->
+                        //TODO : 선택된 컬러로 변경
+                    },
+                    onSizeItemSelected = { selectedValue : Int ->
+                        //TODO : 선택된 사이즈로 변경
+                    })
                 dialog.show()
             }
             R.id.iv_close_menu -> {
--- app/src/main/java/com/example/memoapp/dialog/ThemeDialog.kt
+++ app/src/main/java/com/example/memoapp/dialog/ThemeDialog.kt
@@ -1,20 +1,72 @@
 package com.example.memoapp.dialog
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.example.memoapp.R
+import com.example.memoapp.databinding.ActivityDrawBinding
+import com.example.memoapp.databinding.DialogThemeBinding
 
-class ThemeDialog : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
-        setFinishOnTouchOutside(true)
+class ThemeDialog(
+    context: Activity,
+    onToolItemSelected: (Int) -> Unit,
+    onColorItemSelected: (Int) -> Unit,
+    onSizeItemSelected: (Int) -> Unit
+) : Dialog(context) {
+
+    private var binding: DialogThemeBinding
+
+    init {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        binding = DataBindingUtil.setContentView(context, R.layout.dialog_theme)
         setContentView(R.layout.dialog_theme)
-        val window : WindowManager.LayoutParams = WindowManager.LayoutParams()
-        window.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-        window.dimAmount = 0f;
-        getWindow().attributes = window
+        val params = window!!.attributes
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT
+        params.dimAmount = 0.0f
+        window!!.attributes = params
+        with(binding) {
+            ivPencil.setOnClickListener(onToolClickListener)
+            ivBrush.setOnClickListener(onToolClickListener)
+            ivHighlighter.setOnClickListener(onToolClickListener)
+            ivSpray.setOnClickListener(onToolClickListener)
+            ivEraser.setOnClickListener(onToolClickListener)
+        }
+    }
+
+    private val onToolClickListener = View.OnClickListener() {
+        with(binding) {
+            ivPencil.setImageResource(0)
+            ivBrush.setImageResource(0)
+            ivHighlighter.setImageResource(0)
+            ivSpray.setImageResource(0)
+            ivEraser.setImageResource(0)
+        }
+        when (it.id) {
+            R.id.iv_pencil -> {
+                binding.ivPencil.setImageResource(R.drawable.bg_rectangle_white_blue_line)
+            }
+
+            R.id.iv_brush -> {
+                binding.ivBrush.setImageResource(R.drawable.bg_rectangle_white_blue_line)
+            }
+            R.id.iv_highlighter -> {
+                binding.ivHighlighter.setImageResource(R.drawable.bg_rectangle_white_blue_line)
+            }
+            R.id.iv_spray -> {
+                binding.ivSpray.setImageResource(R.drawable.bg_rectangle_white_blue_line)
+            }
+            R.id.iv_eraser -> {
+                binding.ivEraser.setImageResource(R.drawable.bg_rectangle_white_blue_line)
+            }
+        }
     }
 }
